d639668e693cab6dc5640f864703de21982ccb5c
刘西川 <xichuan.lxc@gmail.com>
2024-08-09 15:55:26 +0800

    dev

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,3 @@
 <template>
-  <header
-    class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4"
-  >
-    <div class="relative ml-auto flex-1 md:grow-0">
-      <Input
-        type="search"
-        placeholder="Search..."
-        class="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-      />
-    </div>
-  </header>
+
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -9,13 +9,13 @@ const routes = useNestedRouteTree();
     <aside
       class="inset-y-0 left-0 z-10 w-16 lg:w-40 flex-col border-r bg-background flex"
     >
-      <nav class="flex flex-col items-center gap-1 px-2 py-5 border-b">
+      <nav class="flex flex-col items-center px-2 py-5 border-b">
         <NuxtLink to="/">
           <div class="flex items-center gap-1">
             <div
               class="group flex h-12 w-12 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground"
             >
-              <img src="/logo_border.svg" class="w-full h-full"/>
+              <img src="/logo_border.svg" class="w-full h-full" />
             </div>
             <span class="hidden font-bold lg:block">酷兔工具</span>
           </div>
